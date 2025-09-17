8955d22fec1965c0ba7d8be6c5e4a468190a11a9
Quentin Proulx <6324569@edu.vaniercollege.qc.ca>
2025-09-17 11:05:02 -0400

    Finish most of UI and made clear button work and register button activate

---
+++ src/lab03/quentinproulx/Lab03QuentinProulx.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab03.quentinproulx;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 6324569
+ */
+public class Lab03QuentinProulx extends Application {
+        Label firstName = new Label("First Name:");
+        Label lastName = new Label("Last Name:");
+        Label email = new Label("Email:");
+        Label password = new Label("Password:");
+        Label message = new Label();
+        
+        TextField firstNameInput = new TextField();
+        TextField lastNameInput = new TextField();
+        TextField emailInput = new TextField();
+        TextField passwordInput = new TextField();
+        
+        Button register = new Button("Register");
+        Button clear = new Button("Clear");
+    
+        GridPane gridPane = new GridPane();
+        BorderPane borderPane = new BorderPane(gridPane);
+        Scene scene = new Scene(borderPane, 600, 500);
+        
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    boolean firstNameFilled = false;
+    boolean lastNameFilled = false;
+    boolean emailFilled = false;
+    boolean passwordFilled = false;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        register.setDisable(true);
+        
+        firstNameInput.setOnKeyTyped(e -> {
+            firstNameFilled = (firstNameInput.getText().length() > 0) ? true : false;
+            
+            setRegister();
+        });
+        
+        lastNameInput.setOnKeyTyped(e -> {
+            lastNameFilled = (lastNameInput.getText().length() > 0) ? true : false;
+            
+            setRegister();
+        });
+        
+        emailInput.setOnKeyTyped(e -> {
+            emailFilled = (emailInput.getText().length() > 0) ? true : false;
+            
+            setRegister();
+        });
+        
+        passwordInput.setOnKeyTyped(e -> {
+            passwordFilled = (passwordInput.getText().length() > 0) ? true : false;
+            
+            setRegister();
+        });
+        
+        clear.setOnMouseClicked(e -> {
+            firstNameInput.setText("");
+            lastNameInput.setText("");
+            emailInput.setText("");
+            passwordInput.setText("");
+            
+            register.setDisable(true);
+        });
+        
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        
+        gridPane.add(firstName, 0, 0);
+        gridPane.add(lastName, 0, 1);
+        gridPane.add(email, 0, 2);
+        gridPane.add(password, 0, 3);
+        gridPane.add(firstNameInput, 1, 0);
+        gridPane.add(lastNameInput, 1, 1);
+        gridPane.add(emailInput, 1, 2);
+        gridPane.add(passwordInput, 1, 3);
+        
+        gridPane.add(register, 0, 4);
+        gridPane.add(clear, 1, 4);
+        
+        stage.setTitle("Lab03");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    
+    private void setRegister() {
+        if (firstNameFilled && lastNameFilled && emailFilled && passwordFilled) {
+            register.setDisable(false);
+        } else {
+            register.setDisable(true);
+        }
+    }
+    
+}
