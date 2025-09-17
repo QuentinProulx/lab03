e93aee7173d9327a250b195a227fe69f2bc480e2
Quentin Proulx <6324569@edu.vaniercollege.qc.ca>
2025-09-17 11:59:09 -0400

    Finished project

--- src/lab03/quentinproulx/Lab03QuentinProulx.java
+++ src/lab03/quentinproulx/Lab03QuentinProulx.java
@@ -25,7 +25,7 @@ public class Lab03QuentinProulx extends Application {
         TextField firstNameInput = new TextField();
         TextField lastNameInput = new TextField();
         TextField emailInput = new TextField();
-        TextField passwordInput = new TextField();
+        PasswordField passwordInput = new PasswordField();
         
         Button register = new Button("Register");
         Button clear = new Button("Clear");
@@ -80,24 +80,59 @@ public class Lab03QuentinProulx extends Application {
         });
         
         register.setOnMouseClicked(e -> {
+            // Checking password
+            boolean hasLetter = false;
+            boolean hasNumber = false;
             
+            for (Character c : passwordInput.getText().toCharArray()) {
+                if (c > 47 && c < 58) {
+                    hasNumber = true;
+                }
+                if ((c > 64 && c < 91) || (c > 96 && c < 123)) {
+                    hasLetter = true;
+                }
+            }
+            boolean password = hasLetter && hasNumber;
+            
+            // Checking email
+            boolean hasAt = false;
+            
+            String address = emailInput.getText();
+            if (address.split("@").length == 2) {
+                hasAt = true;
+                address = address.split("@")[1];
+            }
+            
+            boolean validDom = false;
+            if (address.split("\\.").length == 2 && hasAt) {
+                validDom = true;
+            }
+            boolean email = hasAt && validDom;
+            
+            if (password && email) {
+                message.setText("WELCOME!");
+            } else {
+                message.setText("ERROR IN FIELD(S)");
+            }
         });
         
         gridPane.setAlignment(Pos.CENTER);
         gridPane.setHgap(10);
         gridPane.setVgap(10);
         
-        gridPane.add(firstName, 0, 0);
-        gridPane.add(lastName, 0, 1);
-        gridPane.add(email, 0, 2);
-        gridPane.add(password, 0, 3);
-        gridPane.add(firstNameInput, 1, 0);
-        gridPane.add(lastNameInput, 1, 1);
-        gridPane.add(emailInput, 1, 2);
-        gridPane.add(passwordInput, 1, 3);
+        gridPane.add(firstName, 0, 1);
+        gridPane.add(lastName, 0, 2);
+        gridPane.add(email, 0, 3);
+        gridPane.add(password, 0, 4);
+        gridPane.add(firstNameInput, 1, 1);
+        gridPane.add(lastNameInput, 1, 2);
+        gridPane.add(emailInput, 1, 3);
+        gridPane.add(passwordInput, 1, 4);
         
-        gridPane.add(register, 0, 4);
-        gridPane.add(clear, 1, 4);
+        gridPane.add(register, 0, 5);
+        gridPane.add(clear, 1, 5);
+        
+        gridPane.add(message, 1, 0);
         
         stage.setTitle("Lab03");
         stage.setScene(scene);
